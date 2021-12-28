400138072fc1406104370bd7a27b62d251c72f67
GitHubDavidTorres <david.torres.santander@gmail.com>
2021-12-27 23:41:08 -0300

    master

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
 import Error from './pages/Error';
-import Loading from './components/Loading';
+
 // import components
 import Navbar from './components/Navbar';
 function App() {
